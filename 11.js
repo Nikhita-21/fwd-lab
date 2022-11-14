12194cab44d03e5370fa46df230d0406ca3dd8c1
nikhita <21b01a1255@svecw.edu.in>
2022-11-14 21:46:24 +0530

    first commit

---
+++ 11.js
@@ -0,0 +1,23 @@
+//exercise 11 js
+function validation()
+{
+let name=document.getElementById("name")
+if( name.value.length<6)
+{
+    alert('invalid name')
+}
+let pass=document.getElementById("password")
+
+if(pass.value.length<6)
+{
+    alert("invalid password")
+}
+let phone=document.getElementById("phno").value
+
+if(phone.length>10 || phone.length<10)
+{
+    if(! phone.match(/[1-9][0-9]{9}$/))
+    alert("invalid phno")
+}
+
+}
